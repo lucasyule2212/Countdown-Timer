7e36d3a1de5275649fc4cb6e822121ad7ed2cb7b
lucasyule2212 <lyrma@cin.ufpe.br>
2021-01-07 16:31:33 -0300

    att script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function getName() {
     let eventName = document.getElementById("event").value;
     document.getElementById("title").innerHTML = eventName;
@@ -25,20 +26,29 @@ function getDate() {
             var days = Math.floor(cntDown / (1000 * 60 * 60 * 24));
             var hours = Math.floor((cntDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             var min = Math.floor((cntDown % (1000 * 60 * 60) / (1000 * 60)))
-            var seconds = Math.floor(cntDown % (1000 * 60) / 1000);
+            var seconds = Math.floor(cntDown % (1000 * 60) / 1000); 
 
-            if (days>=0) {
+            var eventOk=true;
+            let title = document.getElementById("title").innerHTML;
+            if (title == "") {             
+                alert("Nome do evento inválido!")
+                clearInterval(x);  
+                eventOk=false;       
+            }            
+            else if (days>=0&&eventOk==true) {
                 document.getElementById("1").innerHTML = days;
                 document.getElementById("2").innerHTML = hours;
                 document.getElementById("3").innerHTML = min;
                 document.getElementById("4").innerHTML = seconds;  
-            }else{
+            }else               
+            {
                 document.getElementById("1").innerHTML = "";
                 document.getElementById("2").innerHTML = "";
                 document.getElementById("3").innerHTML = "";
                 document.getElementById("4").innerHTML = ""; 
                 alert("Quer voltar no tempo é?");
-            }          
+            }  
+                  
             if (cntDown < 0) {
                 clearInterval(x);
                 document.getElementById("display").innerHTML = "DATA ALCANÇADA"
